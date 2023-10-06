e70fc69a2a46533e88abe2a06d09348bce614709
imtiaz <imtiazuddinedu@gmail.com>
2023-10-06 23:18:15 +0600

    update

--- fetch.js
+++ fetch.js
@@ -1,11 +1,10 @@
+ document.getElementById("we").innerHTML=va
+
 fetch("https://jsonplaceholder.typicode.com/todos/1")
 .then((value)=>{
-  return value.json()
+  return value.json();
 })
 .then ((value)=>{
-
-  document.getElementById("me").innerHTML=value.id;
-  document.getElementById("im").innerHTML=value.title;
-  document.getElementById("we").innerHTML=value.userId;
   console.log(value);
+  document.getElementById("we").innerHTML=value.userId;
 })
